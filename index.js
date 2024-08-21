2591ceab0cce024290da6bf1869df02dd9f49828
thuanhoang07 <zz.mr.thuan.zz@gmail.com>
2024-08-22 00:59:05 +0700

    js modify

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-let map;
-
-// Array of markers
-let markers = [
-    {
-        coordinates: { lat: 21.028511, lng: 105.804817 },  // Tọa độ TP HCM
-        iconImage: 'https://img.icons8.com/fluent/48/000000/marker-storm.png',
-        content: '<h4>TP Hồ Chí Minh</h4>'
-    },
-    {
-        coordinates: { lat: 21.028511, lng: 105.804817 }  // Một vị trí khác ở TP HCM
-    }
-]
-
 function initMap() {
     const options = {
-        zoom: 16,
+        zoom: 14,  // Reduced zoom level
         center: { lat: 21.028511, lng: 105.804817 }  // Trung tâm TP HCM
     }
 
@@ -30,66 +16,9 @@ function initMap() {
         })
     })
 
-    for (let i = 0; i< markers.length; i++) {
+    for (let i = 0; i < markers.length; i++) {
         addMarker(markers[i])
     }
 
     drawDirection()
-
-}
-
-function addMarker(prop) {
-    let marker = new google.maps.Marker({
-        position: prop.coordinates,
-        map: map
-    })
-
-    if( prop.iconImage ) {
-        marker.setIcon(prop.iconImage)
-    }
-
-    if( prop.content ) {
-        let information = new google.maps.InfoWindow({
-            content: prop.content
-        })
-
-        marker.addListener("click", function () {
-            information.open(map, marker)
-        })
-    }
-}
-
-function drawDirection() {
-    const directionService = new google.maps.DirectionsService();
-    const directionRenderer = new google.maps.DirectionsRenderer();
-
-    directionRenderer.setMap(map)
-
-    calculationAndDisplayRoute(directionService, directionRenderer)
-
-}
-
-function calculationAndDisplayRoute(directionService, directionRenderer) {
-    const start = { lat: 21.028511, lng: 105.804817 };  // Điểm bắt đầu ở TP HCM
-    const end = { lat: 21.028511, lng: 105.804817 };    // Điểm kết thúc ở TP HCM
-    const request = {
-        origin: start,
-        destination: end,
-        travelMode: google.maps.DirectionsTravelMode.DRIVING
-    }
-
-    directionService.route(request, function (response, status) {
-       if( status === google.maps.DirectionsStatus.OK ) {
-           directionRenderer.setDirections(response)
-           let myRoute = response.routes[0]
-           let txt = ''
-           for (let i = 0; i < myRoute.legs[0].steps.length; i++) {
-               txt += myRoute.legs[0].steps[i].instructions + "<br />"
-           }
-
-           document.getElementById('directions').innerHTML = txt
-
-       }
-    });
-
 }
