01c6ae1715eb17639244211e29a6fd6018abb470
thuanhoang07 <zz.mr.thuan.zz@gmail.com>
2024-08-22 13:46:49 +0700

    80%

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
+let map;
+let marker;
+let LAT = 20.028511;  
+let LON = 105.804817; 
+
 function initMap() {
     const options = {
-        zoom: 14,  // Reduced zoom level
-        center: { lat: 21.028511, lng: 105.804817 }  // Trung tâm TP HCM
+        zoom: 14,
+        center: { lat: LAT, lng: LON } 
     }
 
-    map = new google.maps.Map(
-        document.getElementById('map'),
-        options
-    )
+    map = new google.maps.Map(document.getElementById('map'), options);
 
-    // Listen to map click
-    google.maps.event.addListener(map, 'click', function (event) {
-        addMarker({
-            coordinates: event.latLng
-        })
-    })
+    // Đặt marker ban đầu
+    marker = new google.maps.Marker({
+        position: { lat: LAT, lng: LON },
+        map: map
+    });
 
-    for (let i = 0; i < markers.length; i++) {
-        addMarker(markers[i])
+    // Cập nhật tọa độ của map và marker liên tục mỗi giây
+    setInterval(updateMapCenter, 1000);
+}
+
+function updateMapCenter() {
+    if (LAT && LON) {
+        const newCenter = { lat: parseFloat(LAT), lng: parseFloat(LON) };
+
+        // Cập nhật vị trí trung tâm của map
+        map.setCenter(newCenter);
+
+        // Cập nhật vị trí của marker
+        marker.setPosition(newCenter);
     }
-
-    drawDirection()
 }
