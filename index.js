6adf5f404b8a30f232f772a6e1453f625243294e
thuanhoang07 <zz.mr.thuan.zz@gmail.com>
2024-08-22 16:33:34 +0700

    2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let map;
 let marker;
+let path;
+let pathCoordinates = [];
 let LAT = 20.028511;  
 let LON = 105.804817; 
 
@@ -9,6 +11,7 @@ function initMap() {
         center: { lat: LAT, lng: LON } 
     }
 
+    // Khởi tạo bản đồ
     map = new google.maps.Map(document.getElementById('map'), options);
 
     // Đặt marker ban đầu
@@ -16,6 +19,17 @@ function initMap() {
         position: { lat: LAT, lng: LON },
         map: map
     });
+
+    // Khởi tạo đối tượng Polyline để vẽ đường đi
+    path = new google.maps.Polyline({
+        path: pathCoordinates,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 4
+    });
+
+    path.setMap(map);
 
     // Cập nhật tọa độ của map và marker liên tục mỗi giây
     setInterval(updateMapCenter, 1000);
@@ -30,5 +44,9 @@ function updateMapCenter() {
 
         // Cập nhật vị trí của marker
         marker.setPosition(newCenter);
+
+        // Thêm vị trí mới vào tuyến đường
+        pathCoordinates.push(newCenter);
+        path.setPath(pathCoordinates); // Cập nhật đường đi
     }
 }
